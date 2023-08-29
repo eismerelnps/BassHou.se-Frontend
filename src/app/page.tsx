a2d6681ea9502328c7f7fc8d91f50d27d986112e
Eismer Lobaina <eismerlobaina@gmail.com>
2023-08-28 22:29:44 -0400

    FEAT: Itegrate the fetch to backend
    Now artits shown on the ArtistCard are fetched from online backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,21 @@
 import ArtistList from '@/components/ArtistList/ArtistList'
-import { Artist } from '@/interfaces/Artists'
 
-//get the endpoint of the api bd
-const url = process.env.NEXT_PUBLIC_DB_API_ARTISTS;
 export default async function Home() {
+  //get the endpoint of the api bd
+  const url: string = process.env.NEXT_PUBLIC_DB_API_ARTISTS || '';
 
-  const fetchMenu = () => {
-    return fetch(url, { cache: "no-store" }).then((res) => res.json());
-  };
 
-  const artists = await fetchMenu();
+ const fetchMenu = () => {
+  return fetch(url, { cache: "no-store" }).then((res) => res.json());
+   };
+
+ const artists = await fetchMenu();
 
 
   return (
     <main className="my-5 ">
       <div className='bg-red-500 fixed top-72 my-5 w-full h-20  z-0'></div>
-      {/* <ArtistList  artists={artists } /> */}
-      <ArtistList/>
+       <ArtistList  artists={artists } />
     </main>
   )
 }
