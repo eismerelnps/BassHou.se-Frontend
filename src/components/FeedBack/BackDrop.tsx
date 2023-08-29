90a90c21b4684f65138553fa5db145c8b7164a47
Eismer Lobaina <eismerlobaina@gmail.com>
2023-08-29 00:00:50 -0400

    FEAT: Implemented new components Added DeleteModal and BackDrop components

---
+++ src/components/FeedBack/BackDrop.tsx
@@ -0,0 +1,20 @@
+'use client'
+import { useAppSelector } from "@/hooks";
+import React from "react";
+
+export default function BackDrop() {
+  const { loading } = useAppSelector((state) => state.ui);
+  return (
+    loading && (
+      <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50">
+        <div className="relative">
+          {/* Flecha circular */}
+          <div className="w-8 h-8 rounded-full border-t-4 border-white animate-spin absolute top-0 left-1/2 transform -translate-x-1/2"></div>
+
+          {/* Mensaje */}
+          <p className="text-white text-2xl mt-4">Autenticando...</p>
+        </div>
+      </div>
+    )
+  );
+}
---
+++ src/components/FeedBack/DeleteModal.tsx
@@ -0,0 +1,124 @@
+"use client";
+import { ArrowPathIcon, TrashIcon } from "@heroicons/react/24/outline";
+//Imports from React
+import { useContext } from "react";
+import { Fragment, useRef, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
+
+//fonts
+//import { francois_one } from "@/fonts/francois_one";
+//import { quicksand } from "@/fonts/quicksand";
+//import { gilda_display } from "@/fonts/gilda_display";
+import { ArrowCircleDown } from "heroicons-react";
+import { useRouter } from "next/navigation";
+//import BackDrop from "../backDrop/BackDrop";
+//import Modal from "../dialog/Modal";
+
+import { useAppDispatch, useAppSelector } from '@/hooks';
+import { adminDeleteArtist } from "@/reducers/uiSlice";
+import { startDeletingArtist } from "@/actions/artist";
+
+// import {
+//   finishLoading,
+//   removeError,
+//   setError,
+//   startLoading,
+// } from "@/actions/ui";
+
+
+export default function DeleteModal() {
+  //get the endpoint of the api bd
+  const url = process.env.NEXT_PUBLIC_DB_API_ARTISTS;
+
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+  const cancelButtonRef = useRef(null);
+
+  const { deleteArtist } = useAppSelector((state) => state.ui);
+
+  const handleDelete = () => {
+    //setOpen();
+    dispatch(startDeletingArtist());
+  };
+
+  return (
+    <Transition.Root show={deleteArtist} as={Fragment}>
+      <Dialog
+        as="div"
+        className="relative z-40"
+        initialFocus={cancelButtonRef}
+        onClose={() => adminDeleteArtist(false)}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <ExclamationTriangleIcon
+                        className="h-6 w-6 text-red-600"
+                        aria-hidden="true"
+                      />
+                    </div>
+                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                      <Dialog.Title
+                        as="h3"
+                        className="text-base font-semibold leading-6 text-gray-900"
+                      >
+                        Delete an Artist
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                        Once you perform this action, the artist will no longer be visible in the app and the information cannot be recovered.                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                  <button
+                    type="button"
+                    className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                    onClick={() => handleDelete()}
+                  >
+                    Delete Anyway
+                  </button>
+
+                  <button
+                    type="button"
+                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                    onClick={() => dispatch(adminDeleteArtist(false))}
+                    ref={cancelButtonRef}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+}
