3c40856b2bc95889bd5a74f5dc9e38af87896b54
Eismer Lobaina <eismerlobaina@gmail.com>
2023-08-30 15:52:41 -0400

    Added a null page component to try deploy

--- src/app/(search-bar)/layout.tsx
+++ src/app/(search-bar)/layout.tsx
@@ -1,7 +1,7 @@
 import SearchBar from '@/components/SearchBar/SearchBar'
 import React from 'react'
 
-export default function layout({ children }: { children: undefined }) {
+export default function layout({ children }: { children: React.ReactNode }) {
     return (
         <section>
             <SearchBar />
