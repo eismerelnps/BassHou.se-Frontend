3e7a0360e5dd4ffb238721d39e35f5caff573659
Eismer Lobaina <eismerlobaina@gmail.com>
2023-08-29 22:52:22 -0400

    FEAT: CREATE Editing artist action
    Implemented a new action to handle a async action to edit an artist on DB

--- src/actions/artist.ts
+++ src/actions/artist.ts
@@ -6,9 +6,12 @@ import { RootState, rootReducer } from "@/store/store";
 import { fileUpload } from "@/helpers/fileUpload";
 import { adminEditArtist } from "@/reducers/artistSlice";
 
+const token = process.env.NEXT_PUBLIC_TOKEN;
+const url = process.env.NEXT_PUBLIC_DB_API_ARTISTS;
+
+
 rootReducer
 //get the endpoint of the api bd
-const url = process.env.NEXT_PUBLIC_DB_API_ARTISTS;
 
 
 //To add a new artist to DB
@@ -49,14 +52,51 @@ export const startAddingNewProduct = () => {
 
 
 
+
+//To edit a artist on DB by its ID
+export const startEditingArtist = () => {
+  return async (dispatch: Dispatch<AnyAction>, getState: () => RootState) => {
+    //const { token } = getState().auth;
+    const artist = getState().artist;
+
+    const { _id } = artist;
+
+    fetch(`${url}/${_id}`, {
+      method: "PUT",
+      body: JSON.stringify(artist),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "*/*",
+        "Accept-Encoding": "gzip, deflate, br",
+        Authorization: token,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        dispatch(uiFinishLoading());
+        dispatch(uiSetError(data.message));
+        //router.refresh();
+      })
+
+      .catch((error) => {
+        dispatch(uiFinishLoading());
+        dispatch(
+          uiSetError(
+            "There was an error editing the artist. Please try again."
+          )
+        );
+      });
+  };
+};
+
+
   //To delete a product from the database by its id
 export const startDeletingArtist = () => {
   return async (dispatch: Dispatch<AnyAction>, getState: () => RootState) => {
     //const { token } = getState().auth;
-    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZWQxZmJkMmFkMGM0Y2M0MWVlZGZkYSIsInVzZXJuYW1lIjoiRWlzbWVyIiwicm9sZSI6InN1cGVyYWRtaW4iLCJpYXQiOjE2OTMyNzQ4MjEsImV4cCI6MTY5Mzg3OTYyMX0.JY6hEfMz6_tPYMWLWNjNNAUWjz2IvxNzpswUMnrqyGY'
     const { _id } = getState().artist;
 
-    //console.log( _id );
+    
 
     dispatch(uiStartLoading());
     fetch(`${url}/${_id}`, {
