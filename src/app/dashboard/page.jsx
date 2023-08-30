2fa322b55c9fd173fe9bfae0c9e604a0a8491747
Eismer Lobaina <eismerlobaina@gmail.com>
2023-08-30 16:05:36 -0400

    Route deleted

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -4,7 +4,7 @@ import AdminPanel from "@/components/AdminPanel/AdminPanel";
 //get the endpoint of the api bd
 const artists_url = process.env.NEXT_PUBLIC_DB_API_ARTISTS;
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
    const fetchMenu = () => {
     return fetch(artists_url, { cache: "no-store" }).then((res) => res.json());
    };
