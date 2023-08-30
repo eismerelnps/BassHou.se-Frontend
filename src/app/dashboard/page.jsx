bd2016081c3bdfb63d3fe5aa454f3a4b285992ad
Eismer Lobaina <eismerlobaina@gmail.com>
2023-08-30 16:04:17 -0400

    try

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,12 +1,13 @@
+//'use client'
 import AdminPanel from "@/components/AdminPanel/AdminPanel";
 
 //get the endpoint of the api bd
 const artists_url = process.env.NEXT_PUBLIC_DB_API_ARTISTS;
 
-export default async function DashboardPage() {
-  const fetchMenu = () => {
+export default function DashboardPage() {
+   const fetchMenu = () => {
     return fetch(artists_url, { cache: "no-store" }).then((res) => res.json());
-  };
+   };
   const artists = await fetchMenu();
 
   return <AdminPanel artists={artists} />;
