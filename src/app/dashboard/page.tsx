3fde53cea456754e6f6264b0bedc82b0673af2e5
Eismer Lobaina <eismerlobaina@gmail.com>
2023-08-31 15:39:10 -0400

    FIXED main script for Google Analitics

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,11 @@
+import React from "react";
 import AdminPanel from "@/components/AdminPanel/AdminPanel";
 
-//get the endpoint of the api bd
-const artists_url = process.env.NEXT_PUBLIC_DB_API_ARTISTS || '';
-const requests_url = process.env.NEXT_PUBLIC_DB_API_REQUESTS || '';
 export default async function DashboardPage() {
+  //get the endpoint of the api bd
+  const artists_url = process.env.NEXT_PUBLIC_DB_API_ARTISTS || '';
+  const requests_url = process.env.NEXT_PUBLIC_DB_API_REQUESTS || '';
+
    const fetchArtists = () => {
     return fetch(artists_url, { cache: "no-store" }).then((res) => res.json());
    };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,7 @@ const PersistProvider = dynamic(
 );
 const inter = Inter({ subsets: ["latin"] });
 
+const id = process.env.NEXT_PUBLIC_ANALYTICS_ID;
 
 export default function RootLayout({
   children,
@@ -42,14 +43,14 @@ export default function RootLayout({
     <html lang="en">
       <head>
         {/* <link rel="icon" href="" /> */}
-         <Script src={`https://www.googletagmanager.com/gtag/G-G047DKTEXM`} />
+         <Script src={`https://www.googletagmanager.com/gtag/${id}`}  />
         <Script id="google-analytics">
           {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
  
-          gtag('config', 'G-G047DKTEXM');
+          gtag('config', ${id});
         `}
         </Script> 
         {/* <Script
