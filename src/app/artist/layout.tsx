3d155fc8764088c02287c97817ef0dfcf8918e46
Eismer Lobaina <eismerlobaina@gmail.com>
2023-09-01 21:55:18 -0400

    FIX:the layout of artists screen now is who makes the fetch to pass the artists array to Search Bar and the page component just returns true

--- src/app/artist/layout.tsx
+++ src/app/artist/layout.tsx
@@ -1,11 +1,22 @@
+import ArtistScreen from '@/components/ArtistScreen/ArtistScreen';
 import SearchBar from '@/components/SearchBar/SearchBar'
 import React from 'react'
 
-export default function layout({ children }: { children: React.ReactNode }) {
+//get the endpoint of the api bd
+const artists_url = process.env.NEXT_PUBLIC_DB_API_ARTISTS || '';
+
+const fetchMenu = () => {
+  return fetch(artists_url, { cache: "no-store" }).then((res) => res.json());
+};
+
+
+export default async function layout({ children }: { children: React.ReactNode }) {
+  const artists = await fetchMenu();
+
   return (
     <div>
-      <SearchBar />
-      {children}
+      <SearchBar artists={artists} />
+      <ArtistScreen artists={artists} />
     </div>
   )
 }
--- src/app/artist/page.tsx
+++ src/app/artist/page.tsx
@@ -1,17 +1,3 @@
-
-import React from 'react'
-import ArtistScreen from '@/components/ArtistScreen/ArtistScreen'
-
-//get the endpoint of the api bd
-const artists_url = process.env.NEXT_PUBLIC_DB_API_ARTISTS || '';
-
-
-export default async function ArtistPage() {
-  const fetchMenu = () => {
-    return fetch(artists_url, { cache: "no-store" }).then((res) => res.json());
-  };
-  const artists = await fetchMenu();
-
-  return <ArtistScreen artists={artists} />
-
+export default function page() {
+  return true
 }
