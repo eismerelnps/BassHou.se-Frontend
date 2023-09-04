74e34471e9da1bc2b3503f2168d985df13da9376
Eismer Lobaina <eismerlobaina@gmail.com>
2023-09-04 19:12:53 -0400

    FIX: Z-index on marquee Added z-index to the marquee

--- src/components/Marquee/Marquee.tsx
+++ src/components/Marquee/Marquee.tsx
@@ -4,8 +4,8 @@ import MarqueeItems from './MarqueeItems';
 
 function Marquee() {
     return (
-        <div className=' border-b-[3px] border-slate-950 marquee w-full h-auto py-3.5 overflow-hidden '>
-            <ul className='marquee_content flex list-none  '>
+        <div className='relative border-b-[3px] border-slate-950 bg-white marquee w-full h-auto py-3.5 overflow-hidden '>
+            <ul className='marquee_content flex list-none  z-50'>
              <MarqueeItems/>
              <MarqueeItems/>
              <MarqueeItems/>
