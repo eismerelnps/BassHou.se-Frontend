a01f6b3753ac2e224a2b78cf8ba268ece8ef0a47
Eismer Lobaina <eismerlobaina@gmail.com>
2023-09-04 20:52:29 -0400

    FEAT: Re-style 404 component componet error404 was refactoriced to get the app style

--- src/components/Error/Error404.tsx
+++ src/components/Error/Error404.tsx
@@ -1,23 +1,30 @@
 "use client";
+import Image from "next/image";
 import Link from "next/link";
+
+import sad404 from '../../../public/sad404.svg'
 
 export default function Error404() {
   return (
-    <main className="relative top-72 grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
-      <div className="text-center">
-        <p className="text-base font-semibold text-[#efed84] ">404</p>
-        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-        An error has occurred
-        </h1>
-        <p className="mt-6 text-base leading-7 text-gray-600">
-        Oops, it doesn&apos;t seem to exist or we had an error loading.
+    <main className="h-screen bg-[#efed84] grid  place-items-center px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center ">
+        <div className="flex justify-center">
+          <Image className="" width={150} height={150} src={sad404} alt="404" />
+        </div>
+
+        <h1 className="mt-4 text-3xl font-bold tracking-tight text-slate-950 sm:text-5xl">
+            404
+          </h1>
+
+        <p className="mt-6 text-base leading-7 text-gray-500">
+          Oops, it doesn&apos;t seem to exist or we had an error loading.
         </p>
         <div className="mt-10 flex items-center justify-center gap-x-6">
           <Link
             href="/"
-            className="rounded-md bg-[#efed84] px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-yellow-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-neutral-950"
+            className="rounded-md bg-slate-50 px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-yellow-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-neutral-950"
           >
-            Refresh Page
+            Go To Home
           </Link>
           {/* <a
             href="https://api.whatsapp.com/send?phone=5356062474
