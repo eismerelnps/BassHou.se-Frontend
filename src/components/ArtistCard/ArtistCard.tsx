a1c35242ee96a639574d04ace9a216e6d48a603d
Eismer Lobaina <eismerlobaina@gmail.com>
2023-09-05 23:11:52 -0400

    FIX: fixed carad width & heidth

--- src/components/ArtistCard/ArtistCard.tsx
+++ src/components/ArtistCard/ArtistCard.tsx
@@ -7,6 +7,9 @@ import { Artist } from '@/interfaces/Artists'
 import facebook from '/public/facebook.png'
 import twitter from '/public/twitter.png'
 import website from '/public/website.png'
+
+import no_image from '/public/no_image.jpg'
+
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
@@ -24,9 +27,9 @@ export default function ArtistCard({ item }: { item: Artist }) {
 
   const { artistName, briefDescription, activeSince, images, visible, profiles } = item
   return (
-    <article className={` ${!visible && 'hidden'} cursor-default  flex-initial  basis-full max-w-xs  sm:w-48 sm:basis-1/2 sm:mx-0 px-2  mt-2  pb-4 md:basis-1/3  lg:lg:basis-1/4 xl:basis-1/5   `}>
+    <article className={` ${!visible && 'hidden'} cursor-default  flex-initial  basis-full max-w-xs min-w-xs sm:basis-1/2 sm:mx-0 px-2  mt-2  pb-4 md:basis-1/3  lg:lg:basis-1/4 xl:basis-1/5   `}>
 
-      <button onClick={handleOpenArtist}>
+      <button onClick={handleOpenArtist} className='w-full h-full'>
 
 
         <section className='flex flex-col  h-full bg-white border-2 border-slate-950 shadow shadow-yellow-100/50 hover:shadow-lg hover:rounded hover:shadow-yellow-200/50  transition delay-100'>
@@ -45,18 +48,13 @@ export default function ArtistCard({ item }: { item: Artist }) {
                 )
                 :
                 (
-                  <svg
-                    className="mx-auto h-36 sm:h-36  w-28  text-neutral-300"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
+                  <Image
+                    className='self-center'
+                    src={no_image}
+                    width={155}
+                    height={50}
+                    alt={artistName}
+                  />
                 )
             }
           </article>
@@ -111,7 +109,7 @@ export default function ArtistCard({ item }: { item: Artist }) {
               </p>
             </article>
             <article className='my-1 h-24 '>
-              <p className=' my-0.5 text-justify inline-block  align-middle text-stone-500 text-sm'>{briefDescription}</p>
+              <p className='w-full my-0.5 text-justify inline-block  align-middle text-stone-500 text-sm'>{briefDescription}</p>
             </article>
           </article>
             <article className="flex justify-center   my-2">
